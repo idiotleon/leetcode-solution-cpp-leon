7d9743c6df906af64b39255e418542cca83ea763
Leon <yanglyu.leon.7@gmail.com>
2021-01-22 18:07:42 -0800

    reviews

---
+++ src/lvl2/lc1657/solution_0_linear_scan.cc
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/determine-if-two-strings-are-close/
+//
+// Time Complexity:     O(N * lg(26))
+// Space Complexity:    O(26) ~ O(1)
+//
+// References:
+//  https://leetcode.com/problems/determine-if-two-strings-are-close/discuss/935920/C++-Short-and-Simple-oror-O(-N-)-solution/827020
+//  https://leetcode.com/problems/determine-if-two-strings-are-close/discuss/935920/C%2B%2B-Short-and-Simple-oror-O(-N-)-solution
+#include <vector>;
+#include <string>;
+
+using namespace std;
+
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int> freqs1(26), freqs2(26);
+        
+        for(auto ch : word1)
+            ++freqs1[ch - 'a'];
+        
+        for(auto ch : word2)
+            ++freqs2[ch - 'a'];
+        
+        for(int i = 0; i < 26; ++i){
+            if((freqs1[i] && !freqs2[i]) || (!freqs1[i] && freqs2[i])){
+                return false;
+            }
+        }
+        
+        sort(freqs1.begin(), freqs1.end());
+        sort(freqs2.begin(), freqs2.end());
+        return freqs1 == freqs2;
+    }
+};
---
+++ src/lvl4/lc0161/solution_0_linear_scan.cc
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/one-edit-distance/
+//
+// Time Complexity:     O(max(`lenS`, `lenT`))
+// Space Complexity:    O(1)
+//
+// References:
+//  https://leetcode.com/problems/one-edit-distance/discuss/50108/C%2B%2B-DP
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isOneEditDistance(string s, string t) {
+        const int lenS = s.length(), lenT = t.length();
+        if(lenS > lenT) return isOneEditDistance(t, s);
+        
+        for(int i = 0; i < lenS; ++i){
+            if(s[i] != t[i]){
+                if(lenS == lenT){
+                    return s.substr(i + 1) == t.substr(i + 1);
+                }else{
+                    return s.substr(i) == t.substr(i + 1);
+                }
+            }
+        }
+        
+        return 1 + lenS == lenT;
+    }
+};
