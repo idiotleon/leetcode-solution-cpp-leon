2f755b206dfcee905a7aae58cd6f21c5a875da32
Leon <yanglyu.leon.7@gmail.com>
2021-02-09 22:44:27 -0800

    review

---
+++ src/lvl4/lc0538/solution_0_dfs_recursive.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/convert-bst-to-greater-tree/
+//
+// Time Complexity:     O()
+// Space Complexity:    O()
+//
+// Reference:
+//  https://leetcode.com/problems/convert-bst-to-greater-tree/discuss/100610/c%2B%2B-solution-beats-100
+#include "tree_node.h"
+
+using namespace std;
+
+class Solution
+{
+public:
+    TreeNode *convertBST(TreeNode *root)
+    {
+        dfs(root);
+        return root;
+    }
+
+    void dfs(TreeNode *node)
+    {
+        if (!node)
+            return;
+        if (node->right)
+            dfs(node->right);
+
+        node->val = (cur_sum += node->val);
+        if (node->left)
+            dfs(node->left);
+    }
+
+private:
+    int cur_sum = 0;
+};
