7c730f669c6ff83b1d521f3d03d9a153c5f2fd73
Leon <yanglyu.leon.7@gmail.com>
2021-02-27 17:24:00 -0800

    review

---
+++ src/lvl3/lc0029/with_bit/solution_0_bit_manipulation.cc
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/divide-two-integers/
+//
+// Time Complexity:     O()
+// Space Complexity:    O()
+//
+// Reference:
+//  https://leetcode.com/problems/divide-two-integers/discuss/13407/C%2B%2B-bit-manipulations
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int divide(int dividend, int divisor)
+    {
+        if (dividend == INT_MIN && divisor == -1)
+        {
+            return INT_MAX;
+        }
+
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+
+        int sign = (dividend > 0) ^ (divisor > 0) ? -1 : 1;
+
+        while (dvd >= dvs)
+        {
+            long temp = dvs, m = 1;
+            while (temp << 1 <= dvd)
+            {
+                temp <<= 1;
+                m <<= 1;
+            }
+
+            dvd -= temp;
+            ans += m;
+        }
+
+        return sign * ans;
+    }
+};
