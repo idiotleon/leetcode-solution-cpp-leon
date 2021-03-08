8a2ee9970d54fa06948a2da57bca3e344f749fc1
Leon <yanglyu.leon.7@gmail.com>
2021-03-07 22:32:16 -0800

    review

---
+++ src/ood/lvl2/lc0706/solution_0_list.cc
@@ -0,0 +1,78 @@
+// https://leetcode.com/problems/design-hashmap/
+//
+// Time Complexities:
+//      put()           O()
+//      get()           O()
+//      remove()        O()
+//
+// Space Complexity:    O(`kSize`)
+//
+// Reference:
+//  https://leetcode.com/problems/design-hashmap/discuss/161483/C++-solution-using-vector-of-lists/472514
+//  https://leetcode.com/problems/design-hashmap/discuss/161483/C%2B%2B-solution-using-vector-of-lists
+#include <list>
+#include <vector>
+
+using namespace std;
+
+class MyHashMap
+{
+public:
+    /** Initialize your data structure here. */
+    MyHashMap()
+    {
+        hash.resize(kRange);
+    }
+
+    /** value will always be non-negative. */
+    void put(int key, int value)
+    {
+        auto &cur_list = hash[key % kRange];
+
+        for (auto &cur_pair : cur_list)
+        {
+            if (cur_pair.first == key)
+            {
+                cur_pair.second = value;
+                return;
+            }
+        }
+
+        cur_list.push_back({key, value});
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key)
+    {
+        auto &cur_list = hash[key % kRange];
+
+        for (auto &cur_pair : cur_list)
+        {
+            if (cur_pair.first == key)
+            {
+                return cur_pair.second;
+            }
+        }
+
+        return -1;
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key)
+    {
+        auto &cur_list = hash[key % kRange];
+
+        for (auto &cur_pair : cur_list)
+        {
+            if (cur_pair.first == key)
+            {
+                cur_list.remove(cur_pair);
+                return;
+            }
+        }
+    }
+
+private:
+    constexpr static int kRange = 10000;
+    vector<list<pair<int, int>>> hash;
+};
