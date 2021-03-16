8ccdcf41462a206d0d1d52bbbd355e12d58ff695
Leon <yanglyu.leon.7@gmail.com>
2021-03-15 22:03:53 -0700

    review

---
+++ src/ood/lvl3/lc0535/solution.cc
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/encode-and-decode-tinyurl/
+//
+// Time Complexities:
+// Space Complexity:
+//
+// Reference:
+//  https://leetcode.com/problems/encode-and-decode-tinyurl/discuss/100280/A-true-stateless-one-in-C%2B%2B-(joke)
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl)
+    {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl)
+    {
+        return shortUrl;
+    }
+};
