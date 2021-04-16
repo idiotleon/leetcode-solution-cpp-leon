3de85f0cc2b80a807439b2ac061321685f1cf06f
Leon <yanglyu.leon.7@gmail.com>
2021-04-15 22:53:13 -0700

    review

---
+++ src/lvl1/lc0509/solution_0_dp_0d.cc
@@ -0,0 +1,26 @@
+// @author: Leon
+// https://leetcode.com/problems/fibonacci-number/
+//
+// Time Complexity:     O(`n`)
+// Space Complexity:    O(1)
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n < 1)
+            return 0;
+
+        int fn_2 = 0, fn_1 = 1;
+        int sum = 1;
+
+        for (int i = 2; i <= n; ++i)
+        {
+            sum = fn_2 + fn_1;
+            fn_2 = fn_1;
+            fn_1 = sum;
+        }
+
+        return sum;
+    }
+};
